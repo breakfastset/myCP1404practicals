fdfa2a20a75b742983194f8ccce7ffd1b251a0ae
kangleng <kangleng@gmail.com>
2020-12-10 12:41:38 +0800

    Create Colour Code Dictionary

--- prac_05/hex_colours.py
+++
@@ -1,9 +0,0 @@
-
-colour_to_code = {"azure2": "#e0eeee",  "firebrick1": "#ff3030", "beige": "beige"}
-
-for key, value in colour_to_code.items():
-    print(key, " - ", value)
-
-sorted_colours = sorted(colour_to_code)
-
-print(sorted_colours)
